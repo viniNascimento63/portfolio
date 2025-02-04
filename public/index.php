8fc50d186a968a501b34f7de5e86a07a0c354506
Vinícius Nascimento <vini_sn@outlook.com>
2025-02-04 10:05:10 -0300

    :sparkles: feat: chamando template home

--- public/index.php
+++ public/index.php
@@ -2,15 +2,25 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 //var_dump(__DIR__ . '/vendor/autoload.php');
 require __DIR__ . '/vendor/autoload.php';
 
+// Create App
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
+// Create Twig
+$twig = Twig::create(__DIR__ . '/templates', ['cache' => false]);
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $twig));
+
+$app->get('/', function (Request $request, Response $response) {
+    $view = Twig::fromRequest($request);
+
+    return $view->render($response, 'home.html.twig', ['name' => 'Vinícius']);
 });
 
 $app->run();
