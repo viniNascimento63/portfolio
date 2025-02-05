7b93411d952906f83b34ae52f40604ef7be728dc
Vinícius Nascimento <vini_sn@outlook.com>
2025-02-05 10:50:43 -0300

    :sparkles: feat: nova rota /home

---
+++ routes/route-home.php
@@ -0,0 +1,11 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+
+$app->get('/', function (Request $request, Response $response) {
+    $view = Twig::fromRequest($request);
+
+    return $view->render($response, 'home.html.twig', ['name' => 'Vinícius']);
+});
